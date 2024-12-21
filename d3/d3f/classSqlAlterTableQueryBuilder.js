d38bbf4832dc63f821f9b6e1a5e890298cf5d5c3
christianparpart <christian@parpart.family>
2024-12-21 21:21:22 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@eac82395a02bbf332e5ed4037fb07cb6b09910f3 🚀

--- d3/d3f/classSqlAlterTableQueryBuilder.js
+++ d3/d3f/classSqlAlterTableQueryBuilder.js
@@ -3,7 +3,7 @@ var classSqlAlterTableQueryBuilder =
     [ "RenameTo", "d3/d3f/classSqlAlterTableQueryBuilder.html#ab1b8c373887d75651403c55c742fa3c8", null ],
     [ "AddColumn", "d3/d3f/classSqlAlterTableQueryBuilder.html#af1c09e085373c5183c793716626d09f9", null ],
     [ "AddColumnAsNullable", "d3/d3f/classSqlAlterTableQueryBuilder.html#a760f0371d3c8de1100ba10fd0d3efca6", null ],
-    [ "AlterColumnType", "d3/d3f/classSqlAlterTableQueryBuilder.html#ad87a233533dd0bba411b8483e2f17ef7", null ],
+    [ "AlterColumn", "d3/d3f/classSqlAlterTableQueryBuilder.html#aa2baccd183036dd059fffa77520916fe", null ],
     [ "RenameColumn", "d3/d3f/classSqlAlterTableQueryBuilder.html#aca0ad1e7dc3fc1f17a39246512428dff", null ],
     [ "DropColumn", "d3/d3f/classSqlAlterTableQueryBuilder.html#a0bd2dc01e143714dcf52c14210736cde", null ],
     [ "AddIndex", "d3/d3f/classSqlAlterTableQueryBuilder.html#a4d68a8db517344384d2a02ff0c96d460", null ],
--- search/all_1.js
+++ search/all_1.js
@@ -8,7 +8,7 @@ var searchData=
   ['addindex_5',['AddIndex',['../d3/d3f/classSqlAlterTableQueryBuilder.html#a4d68a8db517344384d2a02ff0c96d460',1,'SqlAlterTableQueryBuilder']]],
   ['adduniqueindex_6',['AddUniqueIndex',['../d3/d3f/classSqlAlterTableQueryBuilder.html#a88046c6bba7c5ef9473e09ac4b2e582a',1,'SqlAlterTableQueryBuilder']]],
   ['all_7',['all',['../dc/dc7/classDataMapper.html#a3c24766350a44e5c1c59f9201bf2fcc4',1,'DataMapper::All()'],['../d3/d09/classHasMany.html#a4e751f046982df12c95fcb456573426a',1,'HasMany::All() const noexcept'],['../d3/d09/classHasMany.html#a4fceffd5ab0319a37ff7158926cde721',1,'HasMany::All() noexcept'],['../df/dd6/classHasManyThrough.html#a0dd54929e7942a919ce0ec5d5890b03f',1,'HasManyThrough::All() const noexcept'],['../df/dd6/classHasManyThrough.html#aacf4f1b9fd8bfae73dce606bb7baf56e',1,'HasManyThrough::All() noexcept'],['../d1/d91/classSqlSelectQueryBuilder.html#ab6b6bbe36ee51d4de422c8681266da3d',1,'SqlSelectQueryBuilder::All()']]],
-  ['altercolumntype_8',['AlterColumnType',['../d3/d3f/classSqlAlterTableQueryBuilder.html#ad87a233533dd0bba411b8483e2f17ef7',1,'SqlAlterTableQueryBuilder']]],
+  ['altercolumn_8',['AlterColumn',['../d3/d3f/classSqlAlterTableQueryBuilder.html#aa2baccd183036dd059fffa77520916fe',1,'SqlAlterTableQueryBuilder']]],
   ['altertable_9',['AlterTable',['../dc/df2/classSqlQueryFormatter.html#a42667232e97c5b3aebbf1c35a360a962',1,'SqlQueryFormatter']]],
   ['an_20odbc_20sql_20api_20for_20c_2023_10',['Lightweight, an ODBC SQL API for C++23',['../index.html',1,'']]],
   ['and_11',['And',['../d2/dca/classSqlWhereClauseBuilder.html#ad1c67751f8aa91d2b78462282a14d856',1,'SqlWhereClauseBuilder']]],
