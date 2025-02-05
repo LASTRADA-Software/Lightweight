0c41e1afcfd641fbde526a8f02d59e5c7919a6ab
Christian Parpart <christian@parpart.family>
2025-02-05 22:19:03 +0100

    [core] SqlColumnTypeDefinitions: Add MakeColumnTypeFromNative(...) as public API
    Signed-off-by: Christian Parpart <christian@parpart.family>

--- src/Lightweight/SqlColumnTypeDefinitions.hpp
+++ src/Lightweight/SqlColumnTypeDefinitions.hpp
@@ -2,7 +2,11 @@
 
 #pragma once
 
+#include <optional>
 #include <variant>
+
+#include <sql.h>
+#include <sqlext.h>
 
 namespace SqlColumnTypeDefinitions
 {
@@ -50,3 +54,48 @@ using SqlColumnTypeDefinition = std::variant<SqlColumnTypeDefinitions::Bigint,
                                              SqlColumnTypeDefinitions::Timestamp,
                                              SqlColumnTypeDefinitions::VarBinary,
                                              SqlColumnTypeDefinitions::Varchar>;
+
+/// Maps ODBC data type (with given @p size and @p precision) to SqlColumnTypeDefinition
+///
+/// @return SqlColumnTypeDefinition if the data type is supported, otherwise std::nullopt
+constexpr std::optional<SqlColumnTypeDefinition> MakeColumnTypeFromNative(int value,
+                                                                          std::size_t size,
+                                                                          std::size_t precision)
+{
+    // Maps ODBC data types to SqlColumnTypeDefinition
+    // See: https://learn.microsoft.com/en-us/sql/odbc/reference/appendixes/sql-data-types?view=sql-server-ver16
+    using namespace SqlColumnTypeDefinitions;
+    // clang-format off
+    switch (value)
+    {
+        case SQL_BIGINT: return Bigint {};
+        case SQL_BINARY: return Binary { size };
+        case SQL_BIT: return Bool {};
+        case SQL_CHAR: return Char { size };
+        case SQL_DATE: return Date {};
+        case SQL_DECIMAL: return Decimal { .precision = precision, .scale = size };
+        case SQL_DOUBLE: return Real {};
+        case SQL_FLOAT: return Real {};
+        case SQL_GUID: return Guid {};
+        case SQL_INTEGER: return Integer {};
+        case SQL_LONGVARBINARY: return VarBinary { size };
+        case SQL_LONGVARCHAR: return Varchar { size };
+        case SQL_NUMERIC: return Decimal { .precision = precision, .scale = size };
+        case SQL_REAL: return Real {};
+        case SQL_SMALLINT: return Smallint {};
+        case SQL_TIME: return Time {};
+        case SQL_TIMESTAMP: return DateTime {};
+        case SQL_TINYINT: return Tinyint {};
+        case SQL_TYPE_DATE: return Date {};
+        case SQL_TYPE_TIME: return Time {};
+        case SQL_TYPE_TIMESTAMP: return DateTime {};
+        case SQL_VARBINARY: return Binary { size };
+        case SQL_VARCHAR: return Varchar { size };
+        case SQL_WCHAR: return NChar { size };
+        case SQL_WLONGVARCHAR: return NVarchar { size };
+        case SQL_WVARCHAR: return NVarchar { size };
+        case SQL_UNKNOWN_TYPE: return std::nullopt;
+        default: return std::nullopt;
+    }
+    // clang-format on
+}
