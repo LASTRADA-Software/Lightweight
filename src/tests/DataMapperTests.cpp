66a5f7e5619e25b13c01af3963dddac804788858
Christian Parpart <christian@parpart.family>
2025-07-10 22:27:16 +0200

    [DataMapper] BelongsTo: Extend API to allow nullable (optional) column values and adjust test respectively
    Signed-off-by: Christian Parpart <christian@parpart.family>

--- src/tests/DataMapperTests.cpp
+++ src/tests/DataMapperTests.cpp
@@ -1740,7 +1740,7 @@ TEST_CASE_METHOD(SqlTestFixture, "TestMessageStruct", "[DataMapper]")
 struct MessageStructTo
 {
     Field<SqlGuid, PrimaryKey::AutoAssign, SqlRealName { "primary_key" }> id;
-    BelongsTo<&MessagesStruct::id, SqlRealName { "log_key" }> log_message {};
+    BelongsTo<&MessagesStruct::id, SqlRealName { "log_key" }, SqlNullable::Null> log_message {};
 };
 
 TEST_CASE_METHOD(SqlTestFixture, "TestMessageStructTo", "[DataMapper]")
@@ -1758,23 +1758,32 @@ TEST_CASE_METHOD(SqlTestFixture, "TestMessageStructTo", "[DataMapper]")
                         SqlForeignKeyReferenceDefinition { .tableName = "MessagesStruct", .columnName = "primary_key" });
     });
 
-    MessagesStruct message {
+    // Create a message to test the BelongsTo relation
+    auto const message = MessagesStruct {
         .id = SqlGuid::TryParse("B16BEF38-5839-11F0-D290-74563C35FB03").value(),
         .timeStamp = SqlDateTime::Now(),
         .message = L"Hello, World!",
     };
-    dm.Create(message);
+    dm.CreateExplicit(message);
 
-    MessageStructTo to1 { .id = SqlGuid::Create(), .log_message = message.id.Value() };
-    dm.Create(to1);
-    SqlStatement(dm.Connection())
-        .ExecuteDirect(
-            R"SQL( INSERT INTO MessageStructTo (primary_key, log_key) VALUES ("B16BEF38-5839-11F0-D290-74563C35FB03", NULL) )SQL");
+    SECTION("Test BelongsTo with non-NULL relation")
+    {
+        auto const to = MessageStructTo { .id = SqlGuid::Create(), .log_message = message.id.Value() };
+        dm.CreateExplicit(to);
+        auto const queriedTo = dm.QuerySingle<MessageStructTo>(to.id).value();
+        REQUIRE(queriedTo.id.Value() == to.id.Value());
+        REQUIRE(queriedTo.log_message.Value().value() == message.id.Value());
+    }
 
-    REQUIRE(dm.QuerySingle<MessageStructTo>(to1.id).value().log_message->id.Value() == message.id.Value());
-
-    auto const guid = SqlGuid::TryParse("B16BEF38-5839-11F0-D290-74563C35FB03").value();
-    REQUIRE(dm.QuerySingle<MessageStructTo>(guid).value().id.Value() == guid);
+    SECTION("Test BelongsTo with NULL relation")
+    {
+        MessageStructTo to { .id = SqlGuid::Create(), .log_message = std::nullopt };
+        dm.Create(to);
+        auto const queriedTo = dm.QuerySingle<MessageStructTo>(to.id).value();
+        REQUIRE(queriedTo.id.Value() == to.id.Value());
+        REQUIRE(!queriedTo.log_message.IsLoaded());
+        REQUIRE(!queriedTo.log_message.Value().has_value());
+    }
 }
 
 // NOLINTEND(bugprone-unchecked-optional-access)
