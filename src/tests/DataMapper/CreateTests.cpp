c81c3e6885dab16f79f3da91c68ec6f213531931
Yaraslau Tamashevich <yaraslau.tamashevich@gmail.com>
2025-08-18 20:45:10 +0200

    Add test for message struct

--- src/tests/DataMapper/CreateTests.cpp
+++ src/tests/DataMapper/CreateTests.cpp
@@ -125,4 +125,26 @@ TEST_CASE_METHOD(SqlTestFixture, "Table with multiple primary keys", "[DataMappe
     CHECK(queriedRecord == record);
 }
 
+struct MessagesStruct
+{
+    Field<SqlGuid, PrimaryKey::AutoAssign, SqlRealName { "primary_key" }> id;
+    Field<SqlDateTime, SqlRealName { "time_stamp" }> timeStamp;
+    Field<SqlDynamicWideString<4000>, SqlRealName { "Message" }> message;
+};
+
+TEST_CASE_METHOD(SqlTestFixture, "TestMessageStruct", "[DataMapper]")
+{
+    auto dm = DataMapper {};
+    dm.CreateTable<MessagesStruct>();
+
+    MessagesStruct message {
+        .id = SqlGuid::Create(),
+        .timeStamp = SqlDateTime::Now(),
+        .message = L"Hello, World!",
+    };
+
+    dm.Create(message);
+    REQUIRE(message.id.Value());
+}
+
 // NOLINTEND(bugprone-unchecked-optional-access)
