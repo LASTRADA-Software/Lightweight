eae4ada067b4920aff22567f6d5750fe4add8fbf
Yaraslaut <Yaraslaut@users.noreply.github.com>
2026-02-18 18:40:23 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@a232d9e23e6afcb0390b904218ddcd03373e4dde 🚀

--- de/db1/classLightweight_1_1Pool_1_1PooledDataMapper.js
+++ de/db1/classLightweight_1_1Pool_1_1PooledDataMapper.js
@@ -2,5 +2,5 @@ var classLightweight_1_1Pool_1_1PooledDataMapper =
 [
     [ "PooledDataMapper", "de/db1/classLightweight_1_1Pool_1_1PooledDataMapper.html#ab3f4130283fddc423a1911a3962e739c", null ],
     [ "operator->", "de/db1/classLightweight_1_1Pool_1_1PooledDataMapper.html#a122267596e346b15d075ca4c347ac97d", null ],
-    [ "get", "de/db1/classLightweight_1_1Pool_1_1PooledDataMapper.html#a6d7b41a1ddbc49899a68230a5d012c82", null ]
+    [ "Get", "de/db1/classLightweight_1_1Pool_1_1PooledDataMapper.html#af18e106d18590f5e6b4f29a04389ecf5", null ]
 ];
