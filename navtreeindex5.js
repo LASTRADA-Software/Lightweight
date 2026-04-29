d3dc2366aed779d8a84cc85b6bc77ddf1db48cd8
Yaraslaut <Yaraslaut@users.noreply.github.com>
2026-04-29 19:07:03 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@a726f782af006b557084ee9ac2e2ec6c9e582771 🚀

--- navtreeindex5.js
+++ navtreeindex5.js
@@ -1,5 +1,6 @@
 var NAVTREEINDEX5 =
 {
+"de/da0/classLightweight_1_1SqlQueryBuilder.html#adeb820498dd1bc9a5c575244e1bdf853":[10,4,0,9],
 "de/da0/classLightweight_1_1SqlQueryBuilder.html#af144019a618b92754fdc3879e226632d":[10,4,0,3],
 "de/da0/classLightweight_1_1SqlQueryBuilder.html#af7ef6f1c5540f6aba2f607f81c7938ec":[10,4,0,2],
 "de/dae/classLightweight_1_1SqlFixedString.html":[10,0,5],
@@ -194,8 +195,8 @@ var NAVTREEINDEX5 =
 "functions_e.html":[12,2,0,5],
 "functions_enum.html":[12,2,4],
 "functions_f.html":[12,2,0,6],
-"functions_func.html":[12,2,1,0],
 "functions_func.html":[12,2,1],
+"functions_func.html":[12,2,1,0],
 "functions_func_b.html":[12,2,1,1],
 "functions_func_c.html":[12,2,1,2],
 "functions_func_d.html":[12,2,1,3],
@@ -248,6 +249,5 @@ var NAVTREEINDEX5 =
 "index.html#compile-using-c26-reflection-support":[0,6],
 "index.html#foreign-keys-relation":[0,3,1],
 "index.html#generate-example-for-the-existing-database":[0,5],
-"index.html#high-level-api":[0,3],
-"index.html#mapping-query-results-to-a-simple-struct":[0,3,2]
+"index.html#high-level-api":[0,3]
 };
