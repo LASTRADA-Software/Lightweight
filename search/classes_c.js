ff5e9950bbce0d6656ce167d770a1063d7312777
christianparpart <christianparpart@users.noreply.github.com>
2026-04-30 17:59:21 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@73cd2b32711a1fef6fa32d3e754b6e2aeb43d274 🚀

--- search/classes_c.js
+++ search/classes_c.js
@@ -3,5 +3,6 @@ var searchData=
   ['table_0',['Table',['../d3/dda/structLightweight_1_1SqlSchema_1_1Table.html',1,'Lightweight::SqlSchema']]],
   ['tablefilter_1',['TableFilter',['../d9/dfd/classLightweight_1_1SqlBackup_1_1TableFilter.html',1,'Lightweight::SqlBackup']]],
   ['tableinfo_2',['TableInfo',['../d9/dfa/structLightweight_1_1SqlBackup_1_1TableInfo.html',1,'Lightweight::SqlBackup']]],
-  ['threadsafequeue_3',['ThreadSafeQueue',['../d4/d9e/classLightweight_1_1ThreadSafeQueue.html',1,'Lightweight']]]
+  ['tablestate_3',['TableState',['../d9/d80/structLightweight_1_1SqlMigration_1_1MigrationManager_1_1PlanFoldingResult_1_1TableState.html',1,'Lightweight::SqlMigration::MigrationManager::PlanFoldingResult']]],
+  ['threadsafequeue_4',['ThreadSafeQueue',['../d4/d9e/classLightweight_1_1ThreadSafeQueue.html',1,'Lightweight']]]
 ];
---
+++ search/variables_14.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['wherecolumn_0',['wherecolumn',['../d4/db0/structLightweight_1_1SqlUpdateDataPlan.html#aeaa5a99453909f230477cff8b907e15e',1,'Lightweight::SqlUpdateDataPlan::whereColumn'],['../d2/d72/structLightweight_1_1SqlDeleteDataPlan.html#a497f383c5c3a27a5056f21546eb14172',1,'Lightweight::SqlDeleteDataPlan::whereColumn']]],
+  ['whereexpression_1',['whereexpression',['../d4/db0/structLightweight_1_1SqlUpdateDataPlan.html#a4ba3ee45a117b2883222f58f6606bbbb',1,'Lightweight::SqlUpdateDataPlan::whereExpression'],['../d2/d72/structLightweight_1_1SqlDeleteDataPlan.html#a0a13f6feba23b295778a7517bd9fc457',1,'Lightweight::SqlDeleteDataPlan::whereExpression']]],
+  ['whereop_2',['whereop',['../d4/db0/structLightweight_1_1SqlUpdateDataPlan.html#ad6ac77aebdac142a100fe41d746fca54',1,'Lightweight::SqlUpdateDataPlan::whereOp'],['../d2/d72/structLightweight_1_1SqlDeleteDataPlan.html#a12c99a15d0f64b3c5b9a1015c29cef84',1,'Lightweight::SqlDeleteDataPlan::whereOp']]],
+  ['wherevalue_3',['wherevalue',['../d4/db0/structLightweight_1_1SqlUpdateDataPlan.html#a308a8fc35020d61bb42ec5733e425f4f',1,'Lightweight::SqlUpdateDataPlan::whereValue'],['../d2/d72/structLightweight_1_1SqlDeleteDataPlan.html#a8e30d14cbde38ffc82f6a713be4ed89d',1,'Lightweight::SqlDeleteDataPlan::whereValue']]],
+  ['writtenfiles_4',['writtenFiles',['../d2/d38/structLightweight_1_1CodeGen_1_1WriteResult.html#a25888e8b0886343d8b5e6eff213f7856',1,'Lightweight::CodeGen::WriteResult']]]
+];
