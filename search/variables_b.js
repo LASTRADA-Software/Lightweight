b71a746d25d1456392322fa685457fcf5802c19c
christianparpart <christianparpart@users.noreply.github.com>
2026-04-30 20:08:50 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@3d73cf049df1a3f10bd00ea4ec7a12a360397671 🚀

--- search/variables_b.js
+++ search/variables_b.js
@@ -3,9 +3,5 @@ var searchData=
   ['length_0',['length',['../d7/d13/structLightweight_1_1SqlRealName.html#afc6f9827f0234476fe8998a3a43259d0',1,'Lightweight::SqlRealName']]],
   ['level_1',['level',['../d9/dd5/structLightweight_1_1SqlBackup_1_1BackupSettings.html#a433db0c5f6bf8d24dc11eec160e262b5',1,'Lightweight::SqlBackup::BackupSettings']]],
   ['libziperror_2',['libzipError',['../da/dd4/structLightweight_1_1Zip_1_1ZipError.html#aae4162b3c01ffe80286bdf62a5561472',1,'Lightweight::Zip::ZipError']]],
-  ['linecount_3',['lineCount',['../d5/d38/structLightweight_1_1CodeGen_1_1CodeBlock.html#a730e061056b96bacf835e26ce4cbd061',1,'Lightweight::CodeGen::CodeBlock']]],
-  ['livetype_4',['liveType',['../d1/d42/structLightweight_1_1SqlMigration_1_1MigrationManager_1_1ColumnUpgradeEntry.html#a396642e22f36188c7ea4dce037c2204e',1,'Lightweight::SqlMigration::MigrationManager::ColumnUpgradeEntry']]],
-  ['loadrelations_5',['loadRelations',['../d3/db2/structLightweight_1_1DataMapperOptions.html#ae7030386be6995fdafef8903818bfe2a',1,'Lightweight::DataMapperOptions']]],
-  ['lookupattempted_6',['lookupAttempted',['../d4/d23/structLightweight_1_1MigrationRenderContext.html#a7d5c90cf0a765b640b1de1ed07a24f28',1,'Lightweight::MigrationRenderContext']]],
-  ['luptruncate_7',['lupTruncate',['../d4/d23/structLightweight_1_1MigrationRenderContext.html#a2951b6980a9d504a748699de0167c8de',1,'Lightweight::MigrationRenderContext']]]
+  ['loadrelations_3',['loadRelations',['../d3/db2/structLightweight_1_1DataMapperOptions.html#ae7030386be6995fdafef8903818bfe2a',1,'Lightweight::DataMapperOptions']]]
 ];
