bfd468e0eb1cdfb9811c46d7b655d5ab5c86289b
christianparpart <christianparpart@users.noreply.github.com>
2026-05-19 19:30:01 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@109a9ed27622141149d495b358c138f5b757a405 🚀

--- search/variables_a.js
+++ search/variables_a.js
@@ -1,11 +1,4 @@
 var searchData=
 [
-  ['length_0',['length',['../d7/d13/structLightweight_1_1SqlRealName.html#afc6f9827f0234476fe8998a3a43259d0',1,'Lightweight::SqlRealName']]],
-  ['level_1',['level',['../d9/dd5/structLightweight_1_1SqlBackup_1_1BackupSettings.html#a433db0c5f6bf8d24dc11eec160e262b5',1,'Lightweight::SqlBackup::BackupSettings']]],
-  ['libziperror_2',['libzipError',['../da/dd4/structLightweight_1_1Zip_1_1ZipError.html#aae4162b3c01ffe80286bdf62a5561472',1,'Lightweight::Zip::ZipError']]],
-  ['livetype_3',['liveType',['../d1/d42/structLightweight_1_1SqlMigration_1_1MigrationManager_1_1ColumnUpgradeEntry.html#a396642e22f36188c7ea4dce037c2204e',1,'Lightweight::SqlMigration::MigrationManager::ColumnUpgradeEntry']]],
-  ['loadrelations_4',['loadRelations',['../d3/db2/structLightweight_1_1DataMapperOptions.html#ae7030386be6995fdafef8903818bfe2a',1,'Lightweight::DataMapperOptions']]],
-  ['lockname_5',['lockName',['../d6/d3b/structLightweight_1_1SqlLockError.html#ac84025dddd1fc79469d13504b5c733cf',1,'Lightweight::SqlLockError']]],
-  ['lookupattempted_6',['lookupAttempted',['../d4/d23/structLightweight_1_1MigrationRenderContext.html#a7d5c90cf0a765b640b1de1ed07a24f28',1,'Lightweight::MigrationRenderContext']]],
-  ['luptruncate_7',['lupTruncate',['../d4/d23/structLightweight_1_1MigrationRenderContext.html#a2951b6980a9d504a748699de0167c8de',1,'Lightweight::MigrationRenderContext']]]
+  ['kind_0',['kind',['../d2/d37/structLightweight_1_1SqlSchema_1_1RowDiff.html#aa58a25f633abe50a3107863b3f1cb59e',1,'Lightweight::SqlSchema::RowDiff::kind'],['../de/d75/structLightweight_1_1SqlSchema_1_1ColumnDiff.html#a5e24af75e41c4ea2a958185ed797e3d5',1,'Lightweight::SqlSchema::ColumnDiff::kind'],['../da/d90/structLightweight_1_1SqlSchema_1_1TableDiff.html#a07a1cf4d040a87a46fa546cb5bbb0d7f',1,'Lightweight::SqlSchema::TableDiff::kind']]]
 ];
