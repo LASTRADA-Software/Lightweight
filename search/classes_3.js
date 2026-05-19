67f49342743e5da43c5f4fee4a2237bcb8ee143b
christianparpart <christianparpart@users.noreply.github.com>
2026-05-19 20:04:57 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@f97fa6f6ed788d0a342071f4557b1fb3230c62f3 🚀

--- search/classes_3.js
+++ search/classes_3.js
@@ -3,6 +3,7 @@ var searchData=
   ['datamapper_0',['DataMapper',['../de/d61/classLightweight_1_1DataMapper.html',1,'Lightweight']]],
   ['datamapperoptions_1',['DataMapperOptions',['../d3/db2/structLightweight_1_1DataMapperOptions.html',1,'Lightweight']]],
   ['datastep_2',['DataStep',['../d8/d8b/structLightweight_1_1SqlMigration_1_1MigrationManager_1_1PlanFoldingResult_1_1DataStep.html',1,'Lightweight::SqlMigration::MigrationManager::PlanFoldingResult']]],
-  ['dropcolumnifexists_3',['DropColumnIfExists',['../d1/d55/structLightweight_1_1SqlAlterTableCommands_1_1DropColumnIfExists.html',1,'Lightweight::SqlAlterTableCommands']]],
-  ['dropindexifexists_4',['DropIndexIfExists',['../d9/dd1/structLightweight_1_1SqlAlterTableCommands_1_1DropIndexIfExists.html',1,'Lightweight::SqlAlterTableCommands']]]
+  ['diffprogressevent_3',['DiffProgressEvent',['../dc/d83/structLightweight_1_1SqlSchema_1_1DiffProgressEvent.html',1,'Lightweight::SqlSchema']]],
+  ['dropcolumnifexists_4',['DropColumnIfExists',['../d1/d55/structLightweight_1_1SqlAlterTableCommands_1_1DropColumnIfExists.html',1,'Lightweight::SqlAlterTableCommands']]],
+  ['dropindexifexists_5',['DropIndexIfExists',['../d9/dd1/structLightweight_1_1SqlAlterTableCommands_1_1DropIndexIfExists.html',1,'Lightweight::SqlAlterTableCommands']]]
 ];
