d2d5feee88c747375913c7dd903d2c9b1e89561d
christianparpart <christianparpart@users.noreply.github.com>
2026-05-20 12:06:23 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@27b46d5974418faa1412385a62f1ef9634aaba04 🚀

--- search/classes_2.js
+++ search/classes_2.js
@@ -6,9 +6,8 @@ var searchData=
   ['chunkwriter_3',['ChunkWriter',['../dd/d06/structLightweight_1_1SqlBackup_1_1ChunkWriter.html',1,'Lightweight::SqlBackup']]],
   ['column_4',['Column',['../d2/db8/structLightweight_1_1SqlSchema_1_1Column.html',1,'Lightweight::SqlSchema']]],
   ['columnbatch_5',['ColumnBatch',['../d1/d6f/structLightweight_1_1SqlBackup_1_1ColumnBatch.html',1,'Lightweight::SqlBackup']]],
-  ['columndiff_6',['ColumnDiff',['../de/d75/structLightweight_1_1SqlSchema_1_1ColumnDiff.html',1,'Lightweight::SqlSchema']]],
-  ['columnidentifier_7',['ColumnIdentifier',['../da/d7c/structLightweight_1_1SqlSchema_1_1ColumnIdentifier.html',1,'Lightweight::SqlSchema']]],
-  ['columnidentifiersequence_8',['ColumnIdentifierSequence',['../d0/dd4/structLightweight_1_1SqlSchema_1_1ColumnIdentifierSequence.html',1,'Lightweight::SqlSchema']]],
-  ['columnkey_9',['ColumnKey',['../da/dcb/structLightweight_1_1MigrationRenderContext_1_1ColumnKey.html',1,'Lightweight::MigrationRenderContext']]],
-  ['columnupgradeentry_10',['ColumnUpgradeEntry',['../d1/d42/structLightweight_1_1SqlMigration_1_1MigrationManager_1_1ColumnUpgradeEntry.html',1,'Lightweight::SqlMigration::MigrationManager']]]
+  ['columnidentifier_6',['ColumnIdentifier',['../da/d7c/structLightweight_1_1SqlSchema_1_1ColumnIdentifier.html',1,'Lightweight::SqlSchema']]],
+  ['columnidentifiersequence_7',['ColumnIdentifierSequence',['../d0/dd4/structLightweight_1_1SqlSchema_1_1ColumnIdentifierSequence.html',1,'Lightweight::SqlSchema']]],
+  ['columnkey_8',['ColumnKey',['../da/dcb/structLightweight_1_1MigrationRenderContext_1_1ColumnKey.html',1,'Lightweight::MigrationRenderContext']]],
+  ['columnupgradeentry_9',['ColumnUpgradeEntry',['../d1/d42/structLightweight_1_1SqlMigration_1_1MigrationManager_1_1ColumnUpgradeEntry.html',1,'Lightweight::SqlMigration::MigrationManager']]]
 ];
--- search/variables_2.js
+++ search/variables_2.js
@@ -1,9 +1,7 @@
 var searchData=
 [
-  ['b_0',['b',['../de/d75/structLightweight_1_1SqlSchema_1_1ColumnDiff.html#a8314a6d6e6f1250139bc9b36e68315e9',1,'Lightweight::SqlSchema::ColumnDiff']]],
-  ['backoffmultiplier_1',['backoffMultiplier',['../d4/d9f/structLightweight_1_1SqlBackup_1_1RetrySettings.html#a2ff324101347d9918088e8a9356f2fec',1,'Lightweight::SqlBackup::RetrySettings']]],
-  ['batchsize_2',['batchSize',['../da/d0d/structLightweight_1_1SqlBackup_1_1RestoreSettings.html#a537031b2f22d3a8b8372965543208aee',1,'Lightweight::SqlBackup::RestoreSettings']]],
-  ['blocksize_3',['BlockSize',['../d2/d43/classLightweight_1_1SqlBackup_1_1Sha256.html#a0b95549d0a226e34ee256fba54134374',1,'Lightweight::SqlBackup::Sha256']]],
-  ['browcount_4',['bRowCount',['../d1/d8b/structLightweight_1_1SqlSchema_1_1TableDataDiff.html#a0c62afaf551d50d6a37a7d43b38d0e2a',1,'Lightweight::SqlSchema::TableDataDiff']]],
-  ['bufferlength_5',['bufferLength',['../da/d07/structLightweight_1_1SqlRawColumnMetadata.html#a7cc9f36813cfe7afbb318d8c0c99963e',1,'Lightweight::SqlRawColumnMetadata']]]
+  ['backoffmultiplier_0',['backoffMultiplier',['../d4/d9f/structLightweight_1_1SqlBackup_1_1RetrySettings.html#a2ff324101347d9918088e8a9356f2fec',1,'Lightweight::SqlBackup::RetrySettings']]],
+  ['batchsize_1',['batchSize',['../da/d0d/structLightweight_1_1SqlBackup_1_1RestoreSettings.html#a537031b2f22d3a8b8372965543208aee',1,'Lightweight::SqlBackup::RestoreSettings']]],
+  ['blocksize_2',['BlockSize',['../d2/d43/classLightweight_1_1SqlBackup_1_1Sha256.html#a0b95549d0a226e34ee256fba54134374',1,'Lightweight::SqlBackup::Sha256']]],
+  ['bufferlength_3',['bufferLength',['../da/d07/structLightweight_1_1SqlRawColumnMetadata.html#a7cc9f36813cfe7afbb318d8c0c99963e',1,'Lightweight::SqlRawColumnMetadata']]]
 ];
