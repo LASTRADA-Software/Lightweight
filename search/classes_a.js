da9613feb99a2364329c6d43114e6270cf5b6f9b
christianparpart <christianparpart@users.noreply.github.com>
2026-06-12 08:19:30 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@a0cf5d5bba15b882dcadcb3421c6d293c377e63e 🚀

--- search/classes_a.js
+++ search/classes_a.js
@@ -3,6 +3,5 @@ var searchData=
   ['restoresettings_0',['RestoreSettings',['../da/d0d/structLightweight_1_1SqlBackup_1_1RestoreSettings.html',1,'Lightweight::SqlBackup']]],
   ['retrysettings_1',['RetrySettings',['../d4/d9f/structLightweight_1_1SqlBackup_1_1RetrySettings.html',1,'Lightweight::SqlBackup']]],
   ['revertresult_2',['RevertResult',['../d1/de7/structLightweight_1_1SqlMigration_1_1RevertResult.html',1,'Lightweight::SqlMigration']]],
-  ['rewritechecksumsresult_3',['RewriteChecksumsResult',['../d2/d24/structLightweight_1_1SqlMigration_1_1MigrationManager_1_1RewriteChecksumsResult.html',1,'Lightweight::SqlMigration::MigrationManager']]],
-  ['rowarraycursor_4',['RowArrayCursor',['../da/d6c/classLightweight_1_1RowArrayCursor.html',1,'Lightweight']]]
+  ['rewritechecksumsresult_3',['RewriteChecksumsResult',['../d2/d24/structLightweight_1_1SqlMigration_1_1MigrationManager_1_1RewriteChecksumsResult.html',1,'Lightweight::SqlMigration::MigrationManager']]]
 ];
