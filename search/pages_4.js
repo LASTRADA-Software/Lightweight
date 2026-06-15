84fb626b94148502356290d0611f4317fe06ad4d
Yaraslaut <Yaraslaut@users.noreply.github.com>
2026-06-15 15:42:17 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@f6c1ecc342080a0e52bf5957f867ae66582110f9 🚀

--- search/pages_4.js
+++ search/pages_4.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['data_20binder_20api_0',['Data Binder API',['../de/db0/md__2home_2runner_2work_2Lightweight_2Lightweight_2docs_2data-binder.html',1,'']]],
   ['database_20management_20cli_1',['dbtool - Database Management CLI',['../d5/dc4/dbtool.html',1,'']]],
-  ['dbtool_20database_20management_20cli_2',['dbtool - Database Management CLI',['../d5/dc4/dbtool.html',1,'']]],
-  ['deprecated_20list_3',['Deprecated List',['../da/d58/deprecated.html',1,'']]]
+  ['dbtool_20database_20management_20cli_2',['dbtool - Database Management CLI',['../d5/dc4/dbtool.html',1,'']]]
 ];
