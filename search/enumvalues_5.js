01fbc3a7562587be1cfdffc7b99ce6ca4db8a193
Yaraslaut <Yaraslaut@users.noreply.github.com>
2026-08-19 14:10:49 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@a8ab53945103530ff2cdb1c23fa9ce5b9f2c10ba 🚀

--- search/enumvalues_5.js
+++ search/enumvalues_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['guid_0',['Guid',['../da/d6c/classLightweight_1_1RowArrayCursor.html#ab61ddfa5d28f1e672f16e9a0a2625fc2ac195f641dd597766b98e4ffa9e2f0e75',1,'Lightweight::RowArrayCursor']]]
+  ['int64_0',['Int64',['../da/d6c/classLightweight_1_1RowArrayCursor.html#ab61ddfa5d28f1e672f16e9a0a2625fc2afbde23b11d7e59af7828e81144c8b487',1,'Lightweight::RowArrayCursor']]]
 ];
