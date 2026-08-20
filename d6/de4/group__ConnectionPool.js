bb45f1ea343fcc1e81413735d66088f0430d56c7
Yaraslaut <Yaraslaut@users.noreply.github.com>
2026-08-20 06:17:01 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@20db36998f383ffa652958ef485b2659e95c45d1 🚀

--- d6/de4/group__ConnectionPool.js
+++ d6/de4/group__ConnectionPool.js
@@ -1,26 +1,18 @@
 var group__ConnectionPool =
 [
     [ "Lightweight::PoolConfig", "df/d1a/structLightweight_1_1PoolConfig.html", [
-      [ "MaxIdleTime", "df/d1a/structLightweight_1_1PoolConfig.html#a090de1dcf0f8f78bb03664ce5f999856", null ],
-      [ "MaxLifetime", "df/d1a/structLightweight_1_1PoolConfig.html#a27ee17aa9854a15e16d385ee1d26b045", null ],
       [ "initialSize", "df/d1a/structLightweight_1_1PoolConfig.html#a8ff366daa9e0eb6c99e5a1e3fc5ec8d4", null ],
       [ "maxSize", "df/d1a/structLightweight_1_1PoolConfig.html#a6a3e753156f65b2aa0ad1fdd0090a72c", null ],
-      [ "growthStrategy", "df/d1a/structLightweight_1_1PoolConfig.html#a82b19e524cdb6dc43c0ec8aab3201e6f", null ],
-      [ "validateOnBorrow", "df/d1a/structLightweight_1_1PoolConfig.html#a5c2abddf40a2e7e08845af824560e5f6", null ],
-      [ "maxIdleTimeMs", "df/d1a/structLightweight_1_1PoolConfig.html#ad9943a108490072e4ac98266abc784a2", null ],
-      [ "maxLifetimeMs", "df/d1a/structLightweight_1_1PoolConfig.html#acad5623b52d85dc1ab20820cd58d8a2f", null ]
+      [ "growthStrategy", "df/d1a/structLightweight_1_1PoolConfig.html#a82b19e524cdb6dc43c0ec8aab3201e6f", null ]
     ] ],
     [ "Lightweight::Pool< Config >", "d3/d30/classLightweight_1_1Pool.html", [
       [ "Pool", "d3/d30/classLightweight_1_1Pool.html#a1cff9d7548fefc8b80a32c15b75dfaeb", null ],
       [ "~Pool", "d3/d30/classLightweight_1_1Pool.html#a33074cde6ef2e6fd95658e3447a2be71", null ],
       [ "Acquire", "d3/d30/classLightweight_1_1Pool.html#ab88d9cb504f2ccbe155f0b8b2b7a0197", null ],
-      [ "Acquire", "d3/d30/classLightweight_1_1Pool.html#a40a00abf0dd52bce81e1d48341d26560", null ],
       [ "Acquire", "d3/d30/classLightweight_1_1Pool.html#a56fdfc02e3f33e88840e66dbe3491f0a", null ],
-      [ "Acquire", "d3/d30/classLightweight_1_1Pool.html#a45b765e8427b198524eefba76daec7d0", null ],
       [ "AcquireAsync", "d3/d30/classLightweight_1_1Pool.html#a8dbac8744491b8abbc8f350c9b8db626", null ],
       [ "SetAsyncExecutors", "d3/d30/classLightweight_1_1Pool.html#ad0b73779e3b92479684324ca2e6b9bf6", null ],
-      [ "AcquireAsync", "d3/d30/classLightweight_1_1Pool.html#aeb3e33321379a9858cd889d664f85d83", null ],
-      [ "SetClock", "d3/d30/classLightweight_1_1Pool.html#aa2f31f6dd54e7ccd110aff57f1fbe66c", null ]
+      [ "AcquireAsync", "d3/d30/classLightweight_1_1Pool.html#aeb3e33321379a9858cd889d664f85d83", null ]
     ] ],
     [ "Lightweight::Pool< Config >::PooledDataMapper", "de/db1/classLightweight_1_1Pool_1_1PooledDataMapper.html", [
       [ "PooledDataMapper", "de/db1/classLightweight_1_1Pool_1_1PooledDataMapper.html#ab3f4130283fddc423a1911a3962e739c", null ],
@@ -31,12 +23,5 @@ var group__ConnectionPool =
       [ "Lightweight::GrowthStrategy::BoundedWait", "d6/de4/group__ConnectionPool.html#ggadf7777be559a52bca78d24e4a99a4f0ca0dd97bbc519abce897d0dd2dc4774411", null ],
       [ "Lightweight::GrowthStrategy::BoundedOverflow", "d6/de4/group__ConnectionPool.html#ggadf7777be559a52bca78d24e4a99a4f0caa081da5fdda27ab1a98aa7e5f2d60d01", null ],
       [ "Lightweight::GrowthStrategy::UnboundedGrow", "d6/de4/group__ConnectionPool.html#ggadf7777be559a52bca78d24e4a99a4f0cab1c6941b5c866676983d4a5195e6f1d7", null ]
-    ] ],
-    [ "Lightweight::ValidateOnBorrow", "d6/de4/group__ConnectionPool.html#ga52b249d4b537b9627a2b2bdd483dd274", [
-      [ "Lightweight::ValidateOnBorrow::No", "d6/de4/group__ConnectionPool.html#gga52b249d4b537b9627a2b2bdd483dd274abafd7322c6e97d25b6299b5d6fe8920b", null ],
-      [ "Lightweight::ValidateOnBorrow::Yes", "d6/de4/group__ConnectionPool.html#gga52b249d4b537b9627a2b2bdd483dd274a93cba07454f06a4a960172bbd6e2a435", null ]
-    ] ],
-    [ "Lightweight::PoolError", "d6/de4/group__ConnectionPool.html#gadf41c4387dc86267bded533482280862", [
-      [ "Lightweight::PoolError::Timeout", "d6/de4/group__ConnectionPool.html#ggadf41c4387dc86267bded533482280862ac85a251cc457840f1e032f1b733e9398", null ]
     ] ]
 ];
