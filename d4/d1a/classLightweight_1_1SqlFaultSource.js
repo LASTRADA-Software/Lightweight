d5c2042312628aeef39e7e8fa7f0427aee8fec0b
Yaraslaut <Yaraslaut@users.noreply.github.com>
2026-09-12 13:03:35 +0000

    Deploying to gh-pages from @ LASTRADA-Software/Lightweight@0e54651a75afa07e3f23ef277bd8239ad83fac5e 🚀

--- d4/d1a/classLightweight_1_1SqlFaultSource.js
+++ d4/d1a/classLightweight_1_1SqlFaultSource.js
@@ -1,4 +1,5 @@
 var classLightweight_1_1SqlFaultSource =
 [
-    [ "NextFailure", "d4/d1a/classLightweight_1_1SqlFaultSource.html#a183b84e14ad19111747cda413e51115d", null ]
+    [ "NextFailure", "d4/d1a/classLightweight_1_1SqlFaultSource.html#a183b84e14ad19111747cda413e51115d", null ],
+    [ "NextConnectionFailure", "d4/d1a/classLightweight_1_1SqlFaultSource.html#a6f883242d7396f3e986625e0c7e8c74f", null ]
 ];
